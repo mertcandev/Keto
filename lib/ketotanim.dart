24d404b1a73b1b6730abde4a47de33dcccfd883a
Mert Can YILMAZ <mertcanylmaz@gmail.com>
2021-12-08 16:43:51 +0300

    Font Size Update

--- lib/ketotanim.dart
+++ lib/ketotanim.dart
@@ -24,7 +24,7 @@ class KetoTanim extends StatelessWidget {
                       "Ketojenik Diyet Nedir?",
                       style: GoogleFonts.firaSans(
                           textStyle: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold)),
+                              fontSize: 17, fontWeight: FontWeight.bold)),
                       textAlign: TextAlign.center,
                       softWrap: true,
                     ),
@@ -38,7 +38,7 @@ class KetoTanim extends StatelessWidget {
                     "Adını, yağ metabolizmasının son ürünleri olan keton cisimciklerinden alan ketojenik diyet, günlük alınan enerjinin en az %60’ı yağ kaynaklarından gelecek şekilde planlanan, karbonhidrat alımını çok çok düşürerek vücudu ketozis isimli metabolik duruma sokmayı amaçlayan, ancak uzman kontrolünde ve tedavi amaçlı kullanıldığı takdirde olumlu etkileri olan bir beslenme modelidir.",
                     style: GoogleFonts.firaSans(
                         textStyle: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.normal)),
+                            fontSize: 15, fontWeight: FontWeight.normal)),
                     textAlign: TextAlign.center,
                   ),
                 ),
@@ -52,7 +52,7 @@ class KetoTanim extends StatelessWidget {
                       "Ketojenik Diyet Ne Yapıyor?",
                       style: GoogleFonts.firaSans(
                           textStyle: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold)),
+                              fontSize: 17, fontWeight: FontWeight.bold)),
                       textAlign: TextAlign.center,
                       softWrap: true,
                     ),
@@ -66,7 +66,7 @@ class KetoTanim extends StatelessWidget {
                     "Bilindiği üzere metabolizmanın öncelikli enerji kaynağı karbonhidratlardır. Ancak yeterli karbonhidrat bulunamadığı takdirde yağlar ve son çare olarak da proteinler kullanılır. Ketojenik beslenme modeli karbonhidratı kesip yerine yağ koyarak vücudun enerji kaynağının yağ olmasını sağlar.",
                     style: GoogleFonts.firaSans(
                         textStyle: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.normal)),
+                            fontSize: 15, fontWeight: FontWeight.normal)),
                     textAlign: TextAlign.center,
                     softWrap: true,
                   ),
@@ -81,7 +81,7 @@ class KetoTanim extends StatelessWidget {
                       "Neden ketojenik diyet?",
                       style: GoogleFonts.firaSans(
                           textStyle: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold)),
+                              fontSize: 17, fontWeight: FontWeight.bold)),
                       textAlign: TextAlign.center,
                       softWrap: true,
                     ),
@@ -95,7 +95,7 @@ class KetoTanim extends StatelessWidget {
                     "Ketojenik diyet aslında bir tedavi yöntemidir. Uzman takibinde, kontrollü aşamalarla uygulanmalı ve gerektiğinde sonlandırılmalıdır. Epilepsi, Alzheimer, Parkinson gibi nörolojik hastalıklar; diyabet, insülin direnci, hipertansiyon, kan yağlarında yükseklik gibi metabolik problemler ve perhiz diyetlerinde kullanılmış ve iyileştirici etkileri birçok çalışmayla kanıtlanmıştır.?",
                     style: GoogleFonts.firaSans(
                         textStyle: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.normal)),
+                            fontSize: 15, fontWeight: FontWeight.normal)),
                     textAlign: TextAlign.center,
                     softWrap: true,
                   ),
@@ -110,7 +110,7 @@ class KetoTanim extends StatelessWidget {
                       "Alzheimer gibi nörolojik hastalıklardaki rolü nedir?",
                       style: GoogleFonts.firaSans(
                           textStyle: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold)),
+                              fontSize: 17, fontWeight: FontWeight.bold)),
                       textAlign: TextAlign.center,
                       softWrap: true,
                     ),
@@ -124,7 +124,7 @@ class KetoTanim extends StatelessWidget {
                     "Alzheimer benzeri bunama hastalıklarının ana sebebi beyin dokusunda bozulmuş karbonhidrat metabolizmasıdır. Günümüzde tip3 diyabet diye de adlandırılan; beyin hücrelerinde insülin direnci oluşması durumuyla, beyin hücreleri enerji kaynağı olarak kullandığı glikozu hücre içine alamadığı için kullanamaz ve enerji üretemez. Hücrelere giremeyen glikozun dokulara baskı yapmasıyla beyin dokusu zarar görürken; enerji üretemeyen dokular hasarlarını onaramaz. Sonuç olarak yaşlanmanın bir kuralı sandığımız demans hastalıkları kendini göstermeye başlar. Ketojenik diyetin bu tedavideki rolü glikozu ortadan kaldırarak beyin hücrelerinin keton cisimciklerinden enerji üretmesini sağlamaktır. Böylece beyin dokusu temiz bir yoldan ve yüksek miktarda enerji üreterek kendini onarmaya olanak bulmuş olur. Bu tip bir tedavi amacıyla kullanılan ketojenik diyet listesi genelde %90 oranında yağdan oluşur.",
                     style: GoogleFonts.firaSans(
                         textStyle: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.normal)),
+                            fontSize: 15, fontWeight: FontWeight.normal)),
                     textAlign: TextAlign.center,
                     softWrap: true,
                   ),
@@ -137,7 +137,7 @@ class KetoTanim extends StatelessWidget {
                     "Metabolizmadaki rolü nedir?",
                     style: GoogleFonts.firaSans(
                         textStyle: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold)),
+                            fontSize: 17, fontWeight: FontWeight.bold)),
                     textAlign: TextAlign.center,
                     softWrap: true,
                   ),
@@ -148,7 +148,7 @@ class KetoTanim extends StatelessWidget {
                   "Ketojenik diyet uygulamasının metabolik sendromu iyileştirmesi esasında kandaki insülin miktarını önemli ölçüde düşürmesinden kaynaklanır. Yani burada da asıl problem insülin direncidir. İnsülin, enerji metabolizmasında çok önemli bir yer tutar ancak yanlış beslenme ve sağlıksız yaşam tarzının kaçınılmaz bir sonu olarak vücut dokularında insülin direnci oluştuğunda, kan şekerinde yükselme, kan yağlarında yükselme, tansiyon yükselmesi, vücudun enerjisiz kalması, şişmanlama ve özellikle de bel çevresinde kalınlaşma problemleri zincirleme olarak birbirini tetikler. Bu problemlerin bütününe metabolik sendrom denir ve metabolik sendrom yaşam biçimindeki değişikliklerle önemli ölçüde iyileştirilebilir. Ketojenik diyetin metabolik sendromda tedavi yöntemi olarak kullanılması da yine karbonhidratı ortadan kaldırarak kandaki insülin miktarını azaltıyor olmasından kaynaklanır. Böylece diyabet hastalarının kilo vermede çektiği zorluk ketojenik diyet uygulamasıyla ortadan kalkar ve sağlıklı biriyle aynı derecede emek harcayarak aynı miktarda kilo kaybı gözlemlenebilir.",
                   style: GoogleFonts.firaSans(
                       textStyle: TextStyle(
-                          fontSize: 14, fontWeight: FontWeight.normal)),
+                          fontSize: 15, fontWeight: FontWeight.normal)),
                   textAlign: TextAlign.center,
                   softWrap: true,
                 ),
@@ -160,7 +160,7 @@ class KetoTanim extends StatelessWidget {
                     "Ketojenik diyet obezetiyi engeller mi?",
                     style: GoogleFonts.firaSans(
                         textStyle: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold)),
+                            fontSize: 17, fontWeight: FontWeight.bold)),
                     textAlign: TextAlign.center,
                     softWrap: true,
                   ),
@@ -171,7 +171,7 @@ class KetoTanim extends StatelessWidget {
                   "Obezite vücuttaki yağ oranının normal aralığın çok üstünde (erkeklerde %25; kadınlarda %30’un üzerinde) olması durumu olarak tanımlanır. Ancak durum bu kadar da tek cümlelik değildir aslında. Yüksek yağ oranı ve hala süregelen sağlıksız yaşam biçimi sebebiyle obez olan bireylerin neredeyse tamamı metabolik problemlere de sahiptir; ya da metabolik problemleri olanlar kilo alırlar... Bu kısır döngü metabolizmayı darmadağın ediyorken bir uzman kontrolünde ketojenik diyet uygulaması onarım ve kilo kaybı için iyi bir yoldur. Üstelik birçok çalışmaya göre ketojenik diyet yapanlar daha az yeme eğilimindedir. Çünkü ketojenik diyetin açlık hormonunun salınımı azaltarak iştahı baskıladığı düşünülmektedir. Tabii burada sorulması gereken soru şudur: Ketojenik diyet obezite tedavisinde kullanılıyorsa kilolu olan ya da kilo vermek isteyen herkes bol yağlı beslenerek ve günlük maksimum 75 g karbonhidrat alarak sağlıklı kilo verebilir mi? Cevap: Hayır! Peki neden?\n1. Ketojenik diyetle sebze-meyveden kısıtlı, baklagilsiz, tahılsız bir beslenme modeli izlemen gerekir. Bu durum da bitkisel kaynaklardan alacağın sayısız faydadan mahrum kalacağın anlamına gelir.\n2. Kontrolsüz bir ketojenik diyet uygulaması ne yiyeceğin ve yemeyeceğin konusunda kafanı karıştırabilir ve ketojenik diyeti bırakmaya karar verdikten sonra karbonhidrata duyduğun açlık nedeniyle daha da kontrolsüz bir beslenme şekli uygulayabilirsin.\n3. Sağlıklı birinin ketojenik diyet uygulaması, normal işleyen metabolizmanın boşu boşuna yorulmasına, vücutta fazla asit birikimine, böbrekler ve karaciğere fazla yük binmesine ve bağırsak problemlerine sebep olabilir.\n4. Uzun vadede kemik erimesi, böbrek yetersizliği gibi problemler oluşabilir. Bu yüzden ketojenik diyet gerektiren bir sağlık problemin olmadıkça bu diyeti uygulamaman ya da kısa süreli ve kontrollü uygulaman daha doğru bir seçim olacaktır.",
                   style: GoogleFonts.firaSans(
                       textStyle: TextStyle(
-                          fontSize: 14, fontWeight: FontWeight.normal)),
+                          fontSize: 15, fontWeight: FontWeight.normal)),
                   textAlign: TextAlign.center,
                   softWrap: true,
                 ),
@@ -183,7 +183,7 @@ class KetoTanim extends StatelessWidget {
                     "Sağlıklı yağlardan korkma, karbonhidratını azalt ama beslenme şeklin “Ketojenik” olmasın!",
                     style: GoogleFonts.firaSans(
                         textStyle: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold)),
+                            fontSize: 17, fontWeight: FontWeight.bold)),
                     textAlign: TextAlign.center,
                     softWrap: true,
                   ),
@@ -196,7 +196,7 @@ class KetoTanim extends StatelessWidget {
                     "Yüksek yağlı ve düşük karbonhidratlı diyetlerin, kısa vadede düşük yağlı diyetlere oranla daha iyi kilo kaybına sebep olduğu pek çok çalışmada gözlenmiş. Kilo vermek için yağdan kaçmana gerek yok. Yüksek yağlı, düşük karbonhidratlı beslenme modeliyle belki kilo verebilir ve iştahını bastırabilirsin, ancak bu diyet ketojenik diyet kadar az karbonhidratlı bir diyet olmamalı ki kilo vereceğim diye sağlığından olma. Sağlıklı yağlar, sağlıklı karbonhidratlar, ihtiyacın kadar protein ve lifle beslenerek istediğin sonuçları alabilirsin. Sağlıklı beslenmenin formülünü aslında hepimiz biliyoruz da maksat hayatımıza uygulamakta!",
                     style: GoogleFonts.firaSans(
                         textStyle: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.normal)),
+                            fontSize: 15, fontWeight: FontWeight.normal)),
                     textAlign: TextAlign.center,
                     softWrap: true,
                   ),
